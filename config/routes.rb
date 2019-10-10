8613199bc08a56c2fe6078bb080a2de8d1cbe1b1
github username <github email address>
2019-10-10 10:54:18 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'hello_world', to: 'static#hello_world'
+#get is the verb, the path is 'hello_world', then route should be passed by the staticcontroller file in controller, hello_world  method
 end
+
+
